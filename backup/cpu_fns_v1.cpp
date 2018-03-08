76698018955e4a2869fa518b2f957c7c16e40536
Agarwal  Pulkit <agarw171@ug71.eecg.toronto.edu>
2018-03-07 21:32:10 -0500

    added cpu_fns v1

--- backup/cpu_fns_v1.cpp
+++ backup/cpu_fns_v1.cpp
@@ -113,12 +113,12 @@ void write_diff_Y(FILE *fid, uint8_t *Y1, uint8_t *Y2){
 int get_sad(uint8_t *Y1, int br1,int bc1,int bf1, uint8_t *Y2, int br2,int bc2,int bf2, bool print=0){
     int val = 0;
     int size2 = B;
-    register int idx1 = bf1*MN +getP(br1)*N +getP(bc1);
-    register int idx2 = bf2*MN +getP(br2)*N +getP(bc2);
-    for (register int ii=0; ii<B; ii++){
-        for (register int jj=0; jj<B; jj++){
-            register int idx1f = idx1 + ii*N + jj;
-            register int idx2f = idx2 + ii*size2 + jj;
+    int idx1 = bf1*MN +getP(br1)*N +getP(bc1);
+    int idx2 = bf2*MN +getP(br2)*N +getP(bc2);
+    for (int ii=0; ii<B; ii++){
+        for (int jj=0; jj<B; jj++){
+            int idx1f = idx1 + ii*N + jj;
+            int idx2f = idx2 + ii*size2 + jj;
 	    if (print) printf("(%d,%d) = %3d %3d \n",idx1f,idx2f,Y1[idx1f], Y2[idx2f] );
             val += abs(Y1[idx1f] - Y2[idx2f]);
         }
@@ -130,14 +130,14 @@ int get_sad(uint8_t *Y1, int br1,int bc1,int bf1, uint8_t *Y2, int br2,int bc2,i
 
 void process_iblock(uint8_t *Yonly, uint8_t *reconstructed, uint8_t *predicted_frame, uint8_t *motion_vector, int br,int bc,int bf){
     uint8_t      local_block      [B][B];
-    register int local_sad;
+    int local_sad;
     uint8_t      lowest_sad_block [B][B];
-    register int lowest_mode;
-    register int lowest_sad; 
+    int lowest_mode;
+    int lowest_sad; 
 
-    register int curr = bf*MN +getP(br)*N +getP(bc);
-    register int left = bf*MN +getP(br)*N +getP(bc-1);
-    register int top  = bf*MN +getP(br-1)*N +getP(bc);
+    int curr = bf*MN +getP(br)*N +getP(bc);
+    int left = bf*MN +getP(br)*N +getP(bc-1);
+    int top  = bf*MN +getP(br-1)*N +getP(bc);
 
     lowest_sad = 147483647;
     lowest_mode = 0;
@@ -208,13 +208,13 @@ void process_iblock(uint8_t *Yonly, uint8_t *reconstructed, uint8_t *predicted_f
 
 void process_pblock(uint8_t *Yonly, uint8_t *reconstructed, uint8_t *predicted_frame, uint8_t *motion_vector, int br,int bc,int bf){
     uint8_t      local_block      [B][B];
-    register int local_sad;
+    int local_sad;
     uint8_t      lowest_sad_block [B][B];
-    register int lowest_sad; 
-    register int lowest_x;
-    register int lowest_y;
+    int lowest_sad; 
+    int lowest_x;
+    int lowest_y;
 
-    register int curr = bf*MN +getP(br)*N +getP(bc);
+    int curr = bf*MN +getP(br)*N +getP(bc);
 
     lowest_sad = 147483647;
     lowest_x   = 0;
@@ -243,7 +243,7 @@ void process_pblock(uint8_t *Yonly, uint8_t *reconstructed, uint8_t *predicted_f
       endy = min(N,getP(bc)+R);
       for (int xx=startx; xx<endx; xx++){
 	for (int yy=starty; yy<endy; yy++){
-	  register int ref = (bf-1)*MN +xx*N +yy;
+	  int ref = (bf-1)*MN +xx*N +yy;
 
 	  // -----for each of the possible blocks, compute the block, sad and compare
 	  for (int ii=0; ii<B; ii++){
